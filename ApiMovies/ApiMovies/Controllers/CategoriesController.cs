c970eec3d397fc5fbdb82f3405da92dc89093015
Camilo Duran <camilod529@gmail.com>
2024-11-20 13:12:39 -0500

    endpoint para la creacion de categorias

--- ApiMovies/ApiMovies/Controllers/CategoriesController.cs
+++ ApiMovies/ApiMovies/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
-﻿using ApiMovies.Models.Dtos;
+﻿using ApiMovies.Models;
+using ApiMovies.Models.Dtos;
 using ApiMovies.Repository.IRepository;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
@@ -49,6 +50,40 @@ namespace ApiMovies.Controllers
             var categoryDto = _mapper.Map<CategoryDto>(category);
             
             return Ok(categoryDto);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult CreateCategory([FromBody] CreateCategoryDto createCategoryDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (createCategoryDto == null)
+            {
+                return BadRequest();
+            }
+
+            if (_categoryRepository.CategoryExists(createCategoryDto.Name))
+            {
+                ModelState.AddModelError("message", $"La categoria {createCategoryDto.Name} ya existe");
+                return BadRequest(ModelState);
+            }
+
+            var category = _mapper.Map<Category>(createCategoryDto);
+
+            if (!_categoryRepository.CreateCategory(category))
+            {
+                ModelState.AddModelError("", $"Algo salio mal guardando el registro {category.Name}");
+                return BadRequest(ModelState);
+            }
+
+            return CreatedAtRoute("GetCategoryById", new {id = category.Id}, category);
         }
     }
 }
