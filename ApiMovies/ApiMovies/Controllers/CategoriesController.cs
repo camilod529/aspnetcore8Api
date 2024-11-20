34025cc6341b461b5467f1b5e5994d85830c8832
Camilo Duran <camilod529@gmail.com>
2024-11-20 12:08:21 -0500

    Creacion de primer endpoint para categorias y agregamos servicios al program

---
+++ ApiMovies/ApiMovies/Controllers/CategoriesController.cs
@@ -0,0 +1,36 @@
+﻿using ApiMovies.Models.Dtos;
+using ApiMovies.Repository.IRepository;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiMovies.Controllers
+{
+    //[Route("api/[controller]")] // Opcion estatica
+    [Route("api/category")] // opcion dinamica
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoriesController(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult getCategories() 
+        {
+            var categories = _categoryRepository.GetAll();
+            var categoriesDto = new List<CategoryDto>();
+            foreach (var category in categories) {
+                categoriesDto.Add(_mapper.Map<CategoryDto>(category));
+            }
+            return Ok(categoriesDto);
+        }
+    }
+}
--- ApiMovies/ApiMovies/Program.cs
+++ ApiMovies/ApiMovies/Program.cs
@@ -1,17 +1,27 @@
 using ApiMovies.Data;
+using ApiMovies.MoviesMappers;
+using ApiMovies.Repository;
+using ApiMovies.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
+
+//Add repositories
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+//Add autommaper
+builder.Services.AddAutoMapper(typeof(MoviesMapper));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
