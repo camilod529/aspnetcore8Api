793674cd92244c0d32ac07556f88e9e6817f3b5c
Camilo Duran <camilod529@gmail.com>
2024-11-20 10:54:38 -0500

    Añadidendo extensiones y modelos para el contexto de base de datos

---
+++ ApiMovies/ApiMovies/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiMovies.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiMovies.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // Todas las entidades (modelos)
+        public DbSet<Category> Categories { get; set; }
+    }
+}
