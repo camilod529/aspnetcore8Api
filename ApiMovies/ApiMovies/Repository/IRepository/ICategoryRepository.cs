4425b2cdba2227249117cbb6ff3c26b670266e28
Camilo Duran <camilod529@gmail.com>
2024-11-20 11:19:30 -0500

    Creada interfaz de categoria

---
+++ ApiMovies/ApiMovies/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using ApiMovies.Models;
+
+namespace ApiMovies.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetAll();
+        Category GetCategoryById(int id);
+        bool CategoryExists(int id);
+        bool CategoryExists(string name);
+        bool CreateCategory(Category category);
+        bool UpdateCategory(Category category);
+        bool DeleteCategory(Category category);
+        bool Save();
+    }
+}
