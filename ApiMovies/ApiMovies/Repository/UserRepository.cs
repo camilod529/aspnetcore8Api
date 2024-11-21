3e9e4dc5b3a93b836b9fcd73ba30e9ac7f1c1d1d
Camilo Duran <camilod529@gmail.com>
2024-11-21 13:18:18 -0500

    Creacion repositorio de usuario

---
+++ ApiMovies/ApiMovies/Repository/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿using ApiMovies.Data;
+using ApiMovies.Models;
+using ApiMovies.Models.Dtos;
+using ApiMovies.Repository.IRepository;
+
+namespace ApiMovies.Repository
+{
+    public class UserRepository : IuserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public User GetUserById(int id)
+        {
+            return _db.Users.FirstOrDefault(x => x.Id == id);
+        }
+
+        public ICollection<User> GetUsers()
+        {
+            return _db.Users.OrderBy(u => u.Name).ToList();
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            var user = _db.Users.FirstOrDefault(u => u.UserName == username);
+            return user == null;
+        }
+
+        public Task<LoginResponseDto> Login(LoginUserDto loginUserDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDataDto> Register(CreateUserDto createUserDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
