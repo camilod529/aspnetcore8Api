7e27c50fdda349bd77781bbc5d95fcb7b28fc533
Camilo Duran <camilod529@gmail.com>
2024-11-21 14:38:23 -0500

    Configuracion para CORS

--- ApiMovies/ApiMovies/Program.cs
+++ ApiMovies/ApiMovies/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>(options => 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
 
 //Add repositories
@@ -23,6 +23,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Soporte para CORS
+builder.Services.AddCors(options =>
+    options.AddPolicy("AllowAll", corsPolicyBuilder =>
+    {
+        corsPolicyBuilder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    })
+);
 
 
 var app = builder.Build();
@@ -33,6 +42,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// CORS
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
