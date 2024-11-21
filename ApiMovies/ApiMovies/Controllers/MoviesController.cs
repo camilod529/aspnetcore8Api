8a354494ce3b96606a90ee7d3f9bb9deb1bb9d03
Camilo Duran <camilod529@gmail.com>
2024-11-21 10:22:57 -0500

    Endpoint de creacion de peliculas

--- ApiMovies/ApiMovies/Controllers/MoviesController.cs
+++ ApiMovies/ApiMovies/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
-﻿using ApiMovies.Models.Dtos;
+﻿using ApiMovies.Models;
+using ApiMovies.Models.Dtos;
 using ApiMovies.Repository;
 using ApiMovies.Repository.IRepository;
 using AutoMapper;
@@ -32,6 +33,60 @@ namespace ApiMovies.Controllers
                 moviesDto.Add(_mapper.Map<MovieDto>(movie));
             }
             return Ok(moviesDto);
+        }
+
+        [HttpGet("{id:int}", Name = "GetMovieById")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetMovieById(int id)
+        {
+            var movie = _movieRepository.GetMovieById(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            var movieDto = _mapper.Map<CategoryDto>(movie);
+
+            return Ok(movieDto);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(MovieDto))]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult CreateMovie([FromBody] CreateMovieDto createMovieDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (createMovieDto == null)
+            {
+                return BadRequest();
+            }
+
+            if (_movieRepository.MovieExists(createMovieDto.Name))
+            {
+                ModelState.AddModelError("message", $"La pelicula {createMovieDto.Name} ya existe");
+                return BadRequest(ModelState);
+            }
+
+            var movie = _mapper.Map<Movie>(createMovieDto);
+
+            if (!_movieRepository.CreateMovie(movie))
+            {
+                ModelState.AddModelError("", $"Algo salio mal guardando el registro {movie.Name}");
+                return BadRequest(ModelState);
+            }
+
+            return CreatedAtRoute("GetMovieById", new { id = movie.Id }, movie);
         }
     }
 }
