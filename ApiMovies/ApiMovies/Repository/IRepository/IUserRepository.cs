1c07b399ae1ce6c3d80d05f675d3e6b9b5673561
Camilo Duran <camilod529@gmail.com>
2024-11-21 13:13:51 -0500

    Creacion interfaz del repositorio de usuario

---
+++ ApiMovies/ApiMovies/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using ApiMovies.Models;
+using ApiMovies.Models.Dtos;
+
+namespace ApiMovies.Repository.IRepository
+{
+    public interface IuserRepository
+    {
+        ICollection<User> GetUsers();
+        User GetUserById(int id);
+        bool IsUniqueUser(string username);
+        Task<LoginResponseDto> Login(LoginUserDto loginUserDto);
+        Task<UserDataDto> Register(CreateUserDto createUserDto);
+    }
+}
