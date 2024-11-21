74f610428d1e90dc3605868b150381abf853f5e6
Camilo Duran <camilod529@gmail.com>
2024-11-21 10:08:38 -0500

    Repositorio e interfaz para Movie

---
+++ ApiMovies/ApiMovies/Repository/IRepository/IMovieRepository.cs
@@ -0,0 +1,18 @@
+﻿using ApiMovies.Models;
+
+namespace ApiMovies.Repository.IRepository
+{
+    public interface IMovieRepository
+    {
+        ICollection<Movie> GetAll();
+        ICollection<Movie> GetMoviesByCategory(int categoryId);
+        ICollection<Movie> SearchMovie(string name);
+        Movie GetMovieById(int id);
+        bool MovieExists(int id);
+        bool MovieExists(string name);
+        bool CreateMovie(Movie movie);
+        bool UpdateMovie(Movie movie);
+        bool DeleteMovie(Movie movie);
+        bool Save();
+    }
+}
---
+++ ApiMovies/ApiMovies/Repository/MovieRepository.cs
@@ -0,0 +1,71 @@
+﻿using ApiMovies.Data;
+using ApiMovies.Models;
+using ApiMovies.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiMovies.Repository
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public bool CreateMovie(Movie movie)
+        {
+            movie.CreatedAt = DateTime.Now;
+            movie.UpdatedAt = null;
+            _db.Movies.Add(movie);
+            return Save();
+        }
+
+        public bool DeleteMovie(Movie movie)
+        {
+            _db.Movies.Remove(movie);
+            return Save();
+        }
+
+        public ICollection<Movie> GetAll()
+        {
+            return _db.Movies.OrderBy(x => x.Name).ToList();
+        }
+
+        public Movie GetMovieById(int id)
+        {
+            return _db.Movies.FirstOrDefault(x => x.Id == id);
+        }
+
+        public ICollection<Movie> GetMoviesByCategory(int categoryId)
+        {
+            return _db.Movies.Include(ca => ca.Category).Where(ca => ca.CategoryId == categoryId).ToList();
+        }
+        public ICollection<Movie> SearchMovie(string name)
+        {
+            IQueryable<Movie> query = _db.Movies;
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                query = query.Where(x => x.Name.Contains(name) || x.Description.Contains(name));
+            }
+
+            return query.ToList();
+        }
+        public bool MovieExists(int id)
+        {
+            return _db.Movies.Any(x => x.Id == id);
+        }
+        public bool MovieExists(string name)
+        {
+            return _db.Movies.Any(y => y.Name.ToLower().Trim() == name.ToLower().Trim());
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0;
+        }
+        public bool UpdateMovie(Movie movie)
+        {
+            movie.UpdatedAt = DateTime.Now;
+            _db.Movies.Update(movie);
+            return Save();
+        }
+    }
+}
