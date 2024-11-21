c79037fb56d54832b2f3bb46c7320b9ce4e0fef5
Camilo Duran <camilod529@gmail.com>
2024-11-21 16:10:14 -0500

    Configuracion de swagger para autorizacion por bearer token

--- ApiMovies/ApiMovies/Program.cs
+++ ApiMovies/ApiMovies/Program.cs
@@ -5,6 +5,7 @@ using ApiMovies.Repository.IRepository;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -45,7 +46,35 @@ builder.Services.AddAuthentication(
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+    {
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Description = "JWT Authorization header using the Bearer scheme. Example: \r\n\r\n" +
+            "\"Bearer qerqweqwewqedads2123\"",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Scheme = "Bearer",
+        });
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                    Scheme = "oauth2",
+                    Name = "Bearer",
+                    In = ParameterLocation.Header
+                },
+                new List<string>()
+            }
+        });
+    }
+);
 
 // Soporte para CORS
 builder.Services.AddCors(options =>
